f770e1dd7d60a8a4064d3f1975237e8c97861c9a
rayderko <rayderko@yahoo.com>
2022-05-03 12:06:58 -0400

    comments
    comments

--- assignments/userinput.js
+++ assignments/userinput.js
@@ -1,33 +1,42 @@
 let input = document.getElementById("user");
 //the above code is where the div element from the html page is brought in,
-//  the first being the input box
+// being the input box
 function helloName() {
     nameVal = input.value; //read what the user typed
     console.log(("Hello ")+ nameVal); //send that info to the console
     input.value="" //clear the input bar
 }
 
-let subtotal = document.getElementById("price");
 
-function tipCal(){
+
+//exercise 2
+
+let subtotal = document.getElementById("price"); 
+
+function tipCal(){ //function that reads in the value of the user and translates it into an integer
     subtotalInt = subtotal.value;
     subtotalInt = parseInt(subtotalInt);
-    tip = subtotalInt * .25;
+    tip = subtotalInt * .25; //this part of the function finds what 25% of the subtotal is and adds it to a variable named tip.
     total = subtotalInt + tip;
-    console.log(("Tip: "+tip+(" total: ")+total))
+    console.log(("Tip: "+tip+(" total: ")+total)) //Both the total variable (sub and tip added) and the tip variable 
+    // get printed and are viewed by the user
 }
+
+
+
+
 let username = document.getElementById("username");
 let password = document.getElementById("password");
 let results = document.getElementById("results");
 
 
-function passwordCheck(){
+function passwordCheck(){ //both values username and password are added together
     usernameVal= username.value;
     passwordVal = password.value;
-    if(usernameVal+passwordVal =="UsernamePassword"){
+    if(usernameVal+passwordVal =="UsernamePassword"){ //if those two values concatinated together equals UsernamePassword, they succeeded
         results.innerHTML="Sucess";}
     else{
-        results.innerHTML="Failed"
+        results.innerHTML="Failed" // if you get anything but what im looking for, fail. 
     }
 }
 
